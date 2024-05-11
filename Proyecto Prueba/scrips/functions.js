b015b8ffcbdfdc428049a7e67c20f5ecd8a3ba39
Christian Pabon <christian75876@gmail.com>
2024-05-10 21:10:01 -0500

    Frist progress

--- Proyecto Prueba/scrips/functions.js
+++ Proyecto Prueba/scrips/functions.js
@@ -1,6 +1,21 @@
 const mostrarTabla = (data)=>{
-    let tabla = document.getElementById("tBody")
+    let tabla = document.getElementById('root')
     tabla.innerHTML = "";
+    tabla.innerHTML = `
+    <table id="usuarios">
+        <thead>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Puntos Acumulados</th>
+            <th>Login</th>
+            <th>Logout</th>
+            <th>Cursos completados</th>
+        </thead>
+        <tbody id="tBody">
+        </tbody>
+    </table>`
+    let tablaBody = document.getElementById('tBody');
+    tablaBody.innerHTML='';
 data.usuarios.forEach(element => {
     let informacion = `
         <tr>
@@ -11,7 +26,7 @@ data.usuarios.forEach(element => {
         <td>${element.datetime_logout}</td>
         <td>${element.cursos_completados}</td>
         </tr>`
-        tabla.innerHTML +=informacion
+        tablaBody.innerHTML +=informacion
     });
 }
 
--- Proyecto Prueba/scrips/main.js
+++ Proyecto Prueba/scrips/main.js
@@ -1,29 +1,33 @@
 import { mostrarTabla, ordenarNombres } from "./functions.js";
 
-const url = './Data/data.json'
+const url = "./Data/data.json";
 
-async function consumirData(){
-    try {
-        const response = await fetch(url)
-        if(!response.ok){
-            throw new Error('Error al cargar los datos')
-        }
-        const data = await response.json();
-        console.table(data)
-        //ordenarNombres(data);
-        
-        mostrarTabla(data);
-        return data 
-    } catch (error) {
-        console.log('Error',error)
-        throw new error
+async function consumirData() {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Error al cargar los datos");
     }
-} 
+    const data = await response.json();
+    console.table(data);
+    //ordenarNombres(data);
 
-consumirData()
+    mostrarTabla(data);
+    return data;
+  } catch (error) {
+    console.log("Error", error);
+    throw new error();
+  }
+}
 
 
 
 
-  
 
+let botom = document.getElementById('root');
+
+botom.innerHTML='';
+botom.innerHTML='<button id="button">Audit</button>';
+
+let btn = document.getElementById('button');
+btn.addEventListener("click",()=> consumirData())
